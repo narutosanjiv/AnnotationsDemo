6a4cdaa8badc4e5de3052387aadbd5bcf86fc240
Sanjiv Jha <sanjiv@joshsoftware.com>
2015-11-16 12:57:42 +0530

    Android Android annotations simple activity demow

---
+++ app/src/main/java/com/android/killua/annotationsdemo/MainActivity.java
@@ -0,0 +1,19 @@
+package com.android.killua.annotationsdemo;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.res.StringRes;
+
